2e7ba31978cba8f828d7a5a5c16f545bf90666b7
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-04 08:38:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,8 +81,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+    programmer_hash =
+    {
+      :yukihiro_matsumoto => {
+ 		      :known_for => "Ruby",
+ 		      :languages => ["LISP", "C"]
+      }
+    }
 end
 
 def changing_alan
