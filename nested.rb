b6b9ae6d5a7aa914217b95d83408bbe1c3aeb4f2
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-04 08:42:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,6 +105,7 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
+        alans_new_info = "GUI"
      }
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
